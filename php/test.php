89cfdf3c6e8e8689dd098a9fd9a5cf5c9eb95c31
keylordmm <keylordmm@gmail.com>
2018-07-26 16:55:19 -0600

    Modal Articulos

--- php/test.php
+++ php/test.php
@@ -1,35 +1,73 @@
 <?php
     include("Menu.php");
 ?>
-<button onclick="$('.ui.modal').modal('show');"class="ui button">
+<button onclick="$('.ui.modal').modal('show');" class="ui button">
   Cancel
 </button>
-<a href="#" onclick="$('.ui.modal').modal('show');">test</a>
-<div class="ui modal">
+<!--Modal Detalles Producto-->
+<div class="ui small modal">
   <i class="close icon"></i>
   <div class="header">
-    Profile Picture
+    Detalles:
   </div>
-  <div class="image content">
+    
+<div  class="image content">
     <div class="ui medium image">
-      <img src="/images/avatar/large/chris.jpg">
+      <img onerror="this.src='../img/desconocido.png';" src="/images/avatar/large/chris.jpg">
     </div>
-    <div class="description">
-      <div class="ui header">We've auto-chosen a profile image for you.</div>
-      <p>We've grabbed the following image from the <a href="https://www.gravatar.com" target="_blank">gravatar</a> image associated with your registered e-mail address.</p>
-      <p>Is it okay to use this photo?</p>
+      
+<div style="padding:0px 0px 0px 3em;" class="ui unstackable items">
+  <div class="item">
+    <div class="content">
+      <a class="ui tiny header">ID Pieza: A23B-8800</a>
     </div>
   </div>
-  <div class="actions">
-    <div class="ui black deny button">
-      Nope
+    
+  <div class="item">
+    <div class="content">
+      <a class="ui tiny header">Categoria: Llantas</a><br>
+        <a class="ui tiny header">ID Categoria: BD48</a>
     </div>
-    <div class="ui positive right labeled icon button">
-      Yep, that's me
-      <i class="checkmark icon"></i>
+  </div>
+    
+<div class="item">
+    <div class="content">
+      <a class="ui tiny header">Descripcion:</a>
+      <div class="description">
+        <p>Descripcion muy corta de un producto imaginario.</p>
+      </div>
+    </div>
+  </div>
+    <div class="item">
+    <div class="content">
+      <a class="ui tiny header">Precio: $100.00</a>
+    </div>
+  </div>
+    
+    <div class="item">
+    <div class="content">
+      <a class="ui tiny header">Observacion(es):</a>
+      <div class="description">
+        <p>Observaciones muy corta de un producto imaginario.</p>
+      </div>
+
+    </div>
+  </div> 
+    <div class="item">
+    <div class="content">
+      <a class="ui tiny header">Estado: Usado</a>
     </div>
   </div>
 </div>
+      
+  </div>
+  <div class="actions">
+    <div class="ui black deny button">
+      Regresar
+    </div>
+  </div>
+</div>
+<!--Modal Detalles Producto-->
  </div>
         </div>      
 
